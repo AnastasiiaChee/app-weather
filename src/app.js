2821f9384c0c2cf2ee73d682c27e7f91164994ea
AnastasiiaChee <beriberibarabara@gmail.com>
2022-07-30 23:17:56 +0300

    Added a search engine

--- src/app.js
+++ src/app.js
@@ -40,8 +40,22 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "5f06a329c299ce37879b66070ebb09f6";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "5f06a329c299ce37879b66070ebb09f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  
+}
+
+search ("New York");
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
